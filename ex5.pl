50ae223c6a7d1e2124493f9a233fdc8d8c9dcc31
EliBenShimol <81102684+EliBenShimol@users.noreply.github.com>
2022-06-26 12:06:26 +0300

    ex5.pl for 3.2

--- ex5.pl
+++ ex5.pl
@@ -13,4 +13,27 @@ maximum_printing_depth(100).
    set_prolog_flag(toplevel_print_options, [max_depth(MPD)|B]).
 
 % Signature: unique(List, UniqueList, Dups)/3
-% Purpose: succeeds if and only if UniqueList contains the same elements of List without duplicates (according to their order in List), and Dups contains the duplicates
+% Purpose: succeeds if and only if UniqueList contains the same elements
+% of List without duplicates (according to their order in List), and
+% Dups contains the duplicate
+
+
+member2(X, [X|_Ys]).
+member2(X, [_Y|Ys]) :- member2(X, Ys).
+
+unique([], [], []).
+unique([X|List], [X|UniqueList], Dups) :- unique(List, UniqueList, Dups),(\+ member2(X, UniqueList)).
+unique([X|List], UniqueList, [X|Dups]) :- unique(List, UniqueList, Dups), member2(X, UniqueList).
+
+
+
+
+
+
+
+
+
+
+
+
+
